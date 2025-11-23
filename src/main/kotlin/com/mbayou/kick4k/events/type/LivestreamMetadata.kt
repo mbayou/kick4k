fae0bc5a03d7fd93cf198eb3377bd7cbf434355d
Mathieu Bayou <mathieu@novasquare.io>
2025-11-23 16:13:02 +0100

    Fix: JSON properties

--- src/main/kotlin/com/mbayou/kick4k/events/type/LivestreamMetadata.kt
+++ src/main/kotlin/com/mbayou/kick4k/events/type/LivestreamMetadata.kt
@@ -1,5 +1,6 @@
 package com.mbayou.kick4k.events.type
 
+import com.fasterxml.jackson.annotation.JsonAlias
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.mbayou.kick4k.categories.Category
@@ -8,5 +9,5 @@ data class LivestreamMetadata @JsonCreator constructor(
     @JsonProperty("title") val title: String,
     @JsonProperty("language") val language: String,
     @JsonProperty("has_mature_content") val hasMatureContent: Boolean,
-    @JsonProperty("category") val category: Category,
+    @JsonProperty("category") @JsonAlias("Category") val category: Category,
 )
