7041efa84e5788ca474603ae176c4d0ac6a3dd9d
Mathieu Bayou <mathieu@novasquare.io>
2025-12-01 13:41:57 +0100

    Fix: LiveStream endpoint for custom tags

--- src/main/kotlin/com/mbayou/kick4k/livestreams/Livestream.kt
+++ src/main/kotlin/com/mbayou/kick4k/livestreams/Livestream.kt
@@ -1,12 +1,15 @@
 package com.mbayou.kick4k.livestreams
 
 import com.fasterxml.jackson.annotation.JsonCreator
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.mbayou.kick4k.categories.Category
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 data class Livestream @JsonCreator constructor(
     @JsonProperty("broadcaster_user_id") val broadcasterUserId: Int?,
     @JsonProperty("category") val category: Category?,
+    @JsonProperty("custom_tags") val customTags: List<String>?,
     @JsonProperty("channel_id") val channelId: Int?,
     @JsonProperty("has_mature_content") val hasMatureContent: Boolean?,
     @JsonProperty("language") val language: String?,
